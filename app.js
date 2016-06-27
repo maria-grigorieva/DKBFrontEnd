60a9e09336656c63184ba747e8c49627128c6d2c
Maria Grigorieva <magsend@gmail.com>
2016-06-27 17:35:43 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,44 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+var exec = require('child_process').exec;
+exec('curl -F "query=SELECT ?document {?document <http://nosql.tpu.ru:8890/DAV/home/dba/ATLAS#documentType> <http://nosql.tpu.ru:8890/DAV/home/dba/ATLAS#SupportingDocuments>}" http://nosql.tpu.ru:8890/sparql -u dba:dba', function(error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
